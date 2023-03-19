436cd7967fd00ce4b8ca0a5eb8fc5b1e34eb0ce8
Fernando Jimenez <fgjv06@gmail.com>
2023-03-19 05:35:29 -0300

    JS - const controls

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 let operators = ["+", "-", "*"];
 const startBtn = document.getElementById("start-btn");
 const question = document.getElementById("question");
+const controls = document.querySelector("controls-container");
