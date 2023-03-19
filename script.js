c797b25351822318e1fe42191978f9d739d4d60f
Fernando Jimenez <fgjv06@gmail.com>
2023-03-19 06:19:50 -0300

    JS - To place the input at random positions

--- script.js
+++ script.js
@@ -27,6 +27,11 @@ const questionGenerator = () => {
   // Solve equation
   let solution = eval(`${num1}${randomOperator}${num2}`);
   console.log(num1, randomOperator, num2, solution);
+
+  // To place the input at random positions
+  // (1 for num1, 2 for num2, 3 for operator, anything else(4) for solution)
+  let randomInpt = randomValue(1, 5);
+  console.log(randomInpt);
 };
   
 // Start Game
